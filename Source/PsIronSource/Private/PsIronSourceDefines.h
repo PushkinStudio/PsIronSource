77f5095ffe0f37078927221e227edd628a9194ea
Anton Rassadin <anton@rassadin.net>
2020-04-02 10:44:10 +0300

    ZEN-4929 Fix headers; Fix copyright notice; Rename log macro

--- Source/PsIronSource/Private/PsIronSourceDefines.h
+++ Source/PsIronSource/Private/PsIronSourceDefines.h
@@ -1,4 +1,4 @@
-// Copyright 2017 Pushkin Studio. All Rights Reserved.
+// Copyright 2015-2020 Mail.Ru Group. All Rights Reserved.
 
 #pragma once
 
@@ -6,6 +6,6 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogPsIronSource, Log, All);
 
-#define PSIS_FUNC (FString(__FUNCTION__))                  // Current Class Name + Function Name where this is called
+#define PS_FUNC (FString(__FUNCTION__))                  // Current Class Name + Function Name where this is called
 #define PSIS_LINE (FString::FromInt(__LINE__))             // Current Line Number in the code where this is called
-#define PSIS_FUNC_LINE (PSIS_FUNC + "(" + PSIS_LINE + ")") // Current Class and Line Number where this is called!
+#define PS_FUNC_LINE (PS_FUNC + "(" + PSIS_LINE + ")") // Current Class and Line Number where this is called!
